7d4d7bd585ba2cf6e559107324f027abab6965f5
Michael Schuetze <mjschuetze102@gmail.com>
2019-07-09 23:55:55 -0400

    Added some additional comments

--- src/AudioPlayer.java
+++ src/AudioPlayer.java
@@ -7,15 +7,24 @@ import java.io.IOException;
  */
 public class AudioPlayer {
 
+    /* Audio file and clip which stores the audio to be played */
     private Clip audioClip;
     private AudioInputStream audioFile;
 
+    /* Variables that keep track of the progress of the current audio being played */
     private boolean audioIsPlaying;
     private Thread currentAudioThread;
 
+    /**
+     * Prepares the audio file so it may be played later
+     * @param fileName - name of the file with the audio data stored
+     */
     public AudioPlayer(String fileName) {
         try {
+            // Locate the audio file
             audioFile = AudioSystem.getAudioInputStream(new File(fileName).getAbsoluteFile());
+            
+            // Open the audio file to be played
             audioClip = AudioSystem.getClip();
             audioClip.open(audioFile);
         } catch (UnsupportedAudioFileException uaf) {
@@ -31,7 +40,11 @@ public class AudioPlayer {
     //              Class Functionality
     /////////////////////////////////////////////////////
 
+    /**
+     * Plays the selected audio file
+     */
     public void play() {
+        // Create a new thread that keeps track of the progress of the audio clip
         currentAudioThread = new Thread(() -> {
             audioClip.start();
 
@@ -41,19 +54,24 @@ public class AudioPlayer {
                     Thread.sleep(10);
                 } catch (InterruptedException ignored) {}
             }
+            
+            // Reset variables needed to play audio
             cleanUp();
         });
         currentAudioThread.start();
     }
 
+    /**
+     * Pauses the currently playing audio
+     */
     public void pause() {
         try {
+            // Prevent the next iteration of the play loop
             audioIsPlaying = false;
+            // Interrupt the thread and wait for it to finish executing
             currentAudioThread.interrupt();
             currentAudioThread.join();
         } catch (InterruptedException ignored) {}
-
-        cleanUp();
     }
 
     public void jumpTo(long timestamp) {}
@@ -67,12 +85,15 @@ public class AudioPlayer {
     /////////////////////////////////////////////////////
 
     private void cleanUp() {
+        // Close the audio stream
         audioClip.stop();
         audioClip.close();
 
+        // Reset variables used to play audio
         audioIsPlaying = false;
         currentAudioThread = null;
 
+        // Close the audio file
         try {
             audioFile.close();
         } catch (IOException ignored) {}
@@ -84,6 +105,7 @@ public class AudioPlayer {
 
     public static void main(String[] args) {
         AudioPlayer audioPlayer = new AudioPlayer("testFile.wav");
+        audioPlayer.pause();
         audioPlayer.play();
         try {
             Thread.sleep(3200);
